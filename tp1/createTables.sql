4d2dcc45b30540e02bd52b5221900518cd312d15
lamdevhs <lam.dev.hs@gmail.com>
2018-09-21 09:26:05 +0200

    TP1: createTable, fillTables

---
+++ tp1/createTables.sql
@@ -0,0 +1,28 @@
+CREATE TABLE client {
+  NUMCLI       int  primary key,
+  NOM          varchar(80),
+  PRENOM       varchar(80),
+  DATENAISS    date,
+  RUE          varchar(120),
+  CP           int,
+  VILLE        varchar(80)
+};
+
+CREATE TABLE fournisseur {
+  NUMFOUR      int primary key,
+  RAISONSOC    varchar(80)
+};
+
+CREATE TABLE produit {
+  NUMPROD      int primary key,
+  DESI         varchar(120),
+  PRIXUNI      real,
+  NUMFOUR      int references fournisseur(NUMFOUR)
+};
+
+CREATE TABLE commande {
+  NUMCLI       int references client(NUMCLI),
+  NUMPROD      int references produit(NUMPROD),
+  QUANTITE     int,
+  DATEC        date
+};
---
+++ tp1/fillTables.sql
@@ -0,0 +1,4 @@
+INSERT INTO client (NUMCLI, NOM, PRENOM, DATENAISS, RUE, CP, VILLE) VALUES
+  (1, 'Dupont', 'Albert', '1970-06-01', 'Rue de Crimee', 69001, 'Lyon'),
+  (2, 'West', 'James', '1963-09-03', 'Studio', 0, 'Hollywood')
+;
